8901103355b099cf268655dbfbe1df19dd3d141a
Robert Gould <rgould.midwest@gmail.com>
2023-08-05 22:31:38 -0500

    working through the bootcamp

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+var topics = ["HTML", "CSS", "GIT", "JavaScript"];
+
+
+/* 
+if (condition) {
+    block of code to be executed if condition is truthy
+} else if (second condition) {
+    block of code to executed if second condition it truthy
+}
+*/
+
+/*
+if (topics[0]) {
+    console.log(topics[0]);    
+} else if(topics==='CSS') {
+    console.log("Let's study CSS!");
+} else if(topics==='GIT') {
+    console.log("Let's study GIT!");
+} else if(topics==='JavaScript') {
+    console.log("Let's study JavaScipt!");
+} else {
+    console.log('Please pick a topic!');
+}
+*/
+  
+//var x = 0;
+
+//x < topics.length;
+
+//x++
+
+// for (initializer; condition (will keep running if true); final-expression) {
+//   code to run each time the loop iterates
+// }
+
+
+
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
